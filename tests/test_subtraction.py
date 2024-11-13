311beffcc16f899e79cb340cb13091ff2e1b52b4
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2024-11-13 17:33:09 +0000

    start of test harness and usage studies

--- tests/test_subtraction.py
+++ tests/test_subtraction.py
@@ -1,11 +1,13 @@
 from pxr import Usd, UsdGeom, Gf,Sdf, G4
 
-
-def defaultBox(box) :
+def setDefaultBox(box) :
     box.InstallUpdateListener()
     box.GetXAttr().Set(5)
     box.GetYAttr().Set(5)
     box.GetZAttr().Set(5)
+
+def setXform(xform) :
+    pass
 
 def test_subtraction() :
     # make subtraction
@@ -14,10 +16,10 @@ def test_subtraction() :
     s = G4.Subtraction.Define(stage, "/s1")
 
     b1 = G4.Box.Define(stage, "/s1/b1")
-    defaultBox(b1)
+    setDefaultBox(b1)
     x = UsdGeom.Xform.Define(stage, "/s1/solid2")
     b2 = G4.Box.Define(stage, "/s1/solid2/b2")
-    defaultBox(b2)
+    setDefaultBox(b2)
 
     stage.Save()
 
